8fcab2904881b13326c0b96a473094890bbba808
Миша Бабенко <misha77b@gmail.com>
2022-07-07 14:51:10 +0300

    home-page styling

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
-import UseStylesHome from './UseStylesHome'
+import React from 'react';
+import UseStylesHome from './UseStylesHome';
+import Button from '@mui/material/Button';
+
+const btnStyles = {
+  fontFamily: "Jost",
+  fontSize: '12px',
+  fontWeight: '500',
+  letterSpacing: '2px',
+  color: '#FF868E',
+  width: '138px',
+  height: '36px',
+  borderRadius: '10px',
+  textAlign: 'center',
+  background: '#FFFFFF',
+}
 
 const Home = () => {
   UseStylesHome()
@@ -13,17 +27,44 @@ const Home = () => {
         <h2 className='home-starts-title'>Lets start using The Cat API</h2>
 
         <div className='home-menu'>
+
           <div className='btn-container'>
-            <img className='vote-table' src="../assets/homepage/vote-table.png" alt="vote-table" />
-            {/* btn */}
+            <div className='menu-image-container vote-container'>
+              <img className='home-image vote-table' src="../assets/homepage/vote-table.png" alt="vote-table" />
+            </div>
+            <Button
+              // onClick={handleOpen}
+              className='vote-btn home-btn'
+              sx={btnStyles}
+            >
+              VOTING
+            </Button>
           </div>
+
           <div className='btn-container'>
-            <img className='pet-breeds' src="../assets/homepage/pet-breeds.png" alt="pet-breeds" />
-            {/* btn */}
+            <div className='menu-image-container breed-container'>
+              <img className='home-image pet-breeds' src="../assets/homepage/pet-breeds.png" alt="pet-breeds" />
+            </div>
+            <Button
+              // onClick={handleOpen}
+              className='breeds-btn home-btn'  
+              sx={btnStyles}          
+            >
+              BREEDS
+            </Button>
           </div>
+
           <div className='btn-container'>
-            <img className='images-search' src="../assets/homepage/images-search.png" alt="images-search" />
-            {/* btn */}
+            <div className='menu-image-container search-container'>
+              <img className='home-image images-search' src="../assets/homepage/images-search.png" alt="images-search" />
+            </div>
+            <Button
+              // onClick={handleOpen}
+              className='search-btn home-btn'
+              sx={btnStyles}
+            >
+              GALLERY
+            </Button>
           </div>
           
           {/* <img className='vote-table' src="../assets/homepage/vote-table.png" alt="vote-table" />
--- src/Components/home/UseStylesHome.js
+++ src/Components/home/UseStylesHome.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@mui/styles';
 
 const UseStylesHome = makeStyles({
     '@global': {
-        '.header': {
+        '.home': {
             // width: '100%',
             // height: '80px',
             // background: 'rgba(10, 10, 10, 0.7)',
@@ -38,19 +38,51 @@ const UseStylesHome = makeStyles({
             color: '#1D1D1D',
         }, 
         '.home-menu': {
-            // have some questions and thoughts
+            // have some questions and thoughts about layout of items
             display: 'grid',
             gridTemplateColumns: '1fr 1fr 1fr',
+            gap: '10px'
         }, 
-        '.vote-table': {
+        '.menu-image-container': {
+            marginBottom: '10px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '138px',
+            height: '198px',
+            border: '2px solid green',
+            borderRadius: '20px',
+        },
+        '.vote-container': {
+            background: '#B4B7FF',
+            border: '4px solid rgba(255, 255, 255, 0.6)',
 
         },
-        '.pet-breeds': {
-
+        '.breed-container': {
+            background: "#97EAB9",
+            border: "4px solid rgba(255, 255, 255, 0.6)",
         },
-        '.images-search': {
-
-        }
+        '.search-container': {
+            background: '#FFD280',
+            border: '4px solid rgba(255, 255, 255, 0.6)',
+        },
+        '.home-btn': {
+            fontFamily: "Jost",
+            fontSize: '12px',
+            fontWeight: '500'
+        },
+        // '.vote-table': {
+        //     width: '117px',
+        //     height: '163px'
+        // },
+        // '.pet-breeds': {
+        //     width: '117px',
+        //     height: '163px'
+        // },
+        // '.images-search': {
+        //     width: '112px',
+        //     height: '190px'
+        // }
     }
 })
 
